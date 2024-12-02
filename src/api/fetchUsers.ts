95ac4324c33eb865a5079759f2e99c4c7a1f1df4
Roman Filonenko <xsspiritttt@gmail.com>
2024-12-02 21:09:23 +0300

    деплой

--- src/api/fetchUsers.ts
+++ src/api/fetchUsers.ts
@@ -2,7 +2,7 @@ import { API_URL } from "@/constants/url";
 
 export async function fetchUsers() {
   try {
-    const res = await fetch(`${API_URL}/users`);
+    const res = await fetch(`${API_URL}/users`, { credentials: "include" });
     return await res.json();
   } catch (e) {
     console.log(e);
