c51db30a5a8dcc978d3cad6fbdd4231ce3a67d8b
Roman Filonenko <xsspiritttt@gmail.com>
2025-06-09 01:30:08 +0300

    chore: add @tanstack/react-query-devtools and update @tanstack/react-query to latest

--- src/app/providers/app-provider.tsx
+++ src/app/providers/app-provider.tsx
@@ -6,11 +6,13 @@ import { ThemeProvider } from "@/app/providers/theme/theme-provider";
 import { FiltersProvider } from "@/app/providers/filters/filters-provider";
 import { ScrollToTop } from "@/app/router/scroll-to-top";
 import { Toaster } from "@/shared/ui/toaster";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export function AppProvider({ children }: PropsWithChildren) {
   const [queryClient] = useState(() => new QueryClient());
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen />
       <AuthProvider>
         <BrowserRouter>
           <ScrollToTop />
